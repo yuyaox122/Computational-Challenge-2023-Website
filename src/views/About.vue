7c6baafa192451a8222097502e4eac9645bf37b1
LAPTOP-TPEQOOPP\yuyao <yuyaoxie122@gmail.com>
2023-07-26 13:47:07 +0100

    First Build

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="overflow-y-scroll scroll-smooth">
         <section
-            class="overflow-hidden container flex items-center justify-center h-screen m-auto bg-fixed bg-center bg-cover bg-[url('src/assets/GalaxiesGalore.jpg')]">
+            class="overflow-hidden container flex items-center justify-center h-screen m-auto bg-fixed bg-center bg-cover bg-[url('/src/assets/GalaxiesGalore.jpg')]">
             <div class="relative group">
                 <div
-                    class="absolute max-w-[82%] max-h-[90%] m-auto -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt">
+                    class="absolute max-w-[82%] max-h-[90%] m-auto -inset-0.5 bg-gradient-to-r from-sky-700 to-blue-800 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt">
                 </div>
-                <img src="src\assets\computational_challenge.png"
+                <img src="/src/assets/computational_challenge.png"
                     class="relative overflow-hidden leading-none rounded-lg m-auto py-10 max-w-[80%] opacity-90">
             </div>
         </section>
         <!-- <header class="relative flex items-center justify-center h-screen overflow-hidden">
             <video class="absolute z-10 w-auto min-w-full min-h-full max-w-none" style="pointer-events: none;" loop="true"
                 autoplay="autoplay" controls muted>
-                <source src="src\assets\SpaceBackground.mp4" type="video/mp4" />
+                <source src="src/assets/SpaceBackground.mp4" type="video/mp4" />
             </video>
             <h1 class="z-30 text-white underline decoration-sky-500/[.33] font-['montserrat'] text-5xl">Task 1</h1>
         </header> -->
--- src/views/Task2.vue
+++ src/views/Task2.vue
@@ -3,14 +3,14 @@
         <header class="relative flex items-center justify-center h-screen overflow-hidden">
             <video class="absolute z-10 w-auto min-w-full min-h-full max-w-none" style="pointer-events: none;" loop="true"
                 autoplay="autoplay" controls muted>
-                <source src="src\assets\SpaceBackground.mp4" type="video/mp4" />
+                <source src="/src/assets/SpaceBackground.mp4" type="video/mp4" />
             </video>
             <h1 class="z-30 text-white underline decoration-sky-500/[.33] font-['montserrat'] text-5xl">Task 2</h1>
         </header>
         <section
             class="overflow-hidden container flex items-center justify-center h-screen m-auto bg-fixed bg-center bg-cover custom-img">
             <video class="z-30 rounded-lg m-auto max-w-[65%]" title="vimeo-player"
-                src="src\assets\Task1.mp4" loop="true"
+                src="/src/assets/Task1.mp4" loop="true"
                 autoplay="autoplay" muted
                 frameborder="0" allowfullscreen controls></video>
         </section>
@@ -18,8 +18,8 @@
             class="overflow-hidden container flex items-center justify-center h-screen m-auto bg-fixed bg-center bg-cover custom-img">
             <p class="font-['montserrat'] text-xl">How did we do this?</p>
         </section>
-        <section class="overflow-hidden container flex items-center justify-center m-auto bg-fixed bg-cover bg-[url('src/assets/SombreroGalaxy.jpg')]">
-            <img src="src\assets\task_2_code.png" class="rounded-lg m-auto py-10 max-w-[80%] opacity-90">
+        <section class="overflow-hidden container flex items-center justify-center m-auto bg-fixed bg-cover bg-[url('/src/assets/SombreroGalaxy.jpg')]">
+            <img src="/src/assets/task_2_code.png" class="rounded-lg m-auto py-10 max-w-[80%] opacity-90">
         </section> 
     </div>
 </template>
